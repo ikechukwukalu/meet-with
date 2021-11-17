0e33319f71115c7d93efa1f97d39fb3e81ac9875
ikechukwu kalu <ea.ikechukwukalu@gmail.com>
2021-11-17 19:41:59 +0100

    Next up - program slots

--- resources/views/livewire/programs.blade.php
+++ resources/views/livewire/programs.blade.php
@@ -1,6 +1,26 @@
 <div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <strong>Success!</strong> {{ session('success') }}
+            </div>
+            @endif
+            @if (session()->has('fail'))
+            <div class="alert alert-danger alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <strong>Failed!</strong> {{ session('fail') }}
+            </div>
+            @endif
+        </div>
+    </div>
+    <p>
+        @error($name) <span class="error">{{ $message }}</span> @enderror
+    </p>
     <form class="form-inline" wire:submit.prevent="addProgram">
-        <input type="text" class="form-control pr-2 " wire:model="name" placeholder="Program Name">&nbsp;
+        <input type="text" class="form-control @error($name) is-invalid @enderror pr-2 " wire:model="name"
+            placeholder="Program Name">&nbsp;
         <button type="submit" class="btn btn-primary" wire:target="addProgram" wire.loading.attr="disabled">
             <span wire:loading="" wire:target="addProgram">
                 <span class="spinner-border spinner-border-sm"></span>&nbsp;Loading...
@@ -16,9 +36,10 @@
             <div class="col-md-12">
                 <ul class="list-group">
                     @forelse($programs as $program)
-                        <li class="list-group-item">
-                            <b>{{ $loop->iteration }}.</b> {{ $program->name }} <button type="button" class="btn btn-clear"></button>
-                        </li>
+                    <li class="list-group-item">
+                        <b>{{ $loop->iteration }}.</b> {{ $program->name }} <button type="button"
+                            class="btn btn-clear"></button>
+                    </li>
                     @empty
                     <p align="center">No Program has be added to this meeting</p>
                     @endforelse
